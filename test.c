ea4f6fed672caf3e9a3e56b538d623b6c6f9e93e
komorebi886 <1603255516@qq.com>
2026-08-20 17:34:53 +0800

    sixth day

--- test.c
+++ test.c
@@ -278,32 +278,66 @@ EOF  -end of file 文件的结束标志 值是-1
 // }
 
 
-// define定义标识符常量
-#define NUM 100
+// // define定义标识符常量
+// #define NUM 100
 
 
-// #define 定义宏
-// 宏是有参数
+// // #define 定义宏
+// // 宏是有参数
 
-#define ADD(x,y) ((x)+(y))
+// #define ADD(x,y) ((x)+(y))
 
-int Add(int x, int y){
-    return x+y;
-}
+// int Add(int x, int y){
+//     return x+y;
+// }
+
+
+// int main(){
+//     printf("%d\n",NUM);
+//     int n=NUM;
+//     printf("%d\n",n);
+//     int arr[NUM]={0};
+//     int a=10;
+//     int b=20;
+//     int c=ADD(a,b);
+
+//     printf("%d",c);
+    
+//     return 0;
+// }
+
+
+// int main(){
+//     int a=10;//向内存申请4个字节，存储10
+//     //&a;//取地址操作符
+//     //printf("%p\n",&a);//0x0012ff40
+//     int * p=&a;
+//     //p就是指针变量
+//     // *p=20;//解引用操作符，意思就是通过p中存放的地址，找到p所指的对象，*p就是p指向的对象
+
+//     printf("%d\n",a);
+
+//     // char ch='w';
+//     // char * pc=&ch;
+
+//     return 0;
+// }
 
 
 int main(){
-    printf("%d\n",NUM);
-    int n=NUM;
-    printf("%d\n",n);
-    int arr[NUM]={0};
-    int a=10;
-    int b=20;
-    int c=ADD(a,b);
+    int * p;
+    char * p2;
+    // 不管是什么类型的指针，都是在创建指针变量
+    // 指针变量是用来存放地址的
+    // 指针变量的大小取决于一个地址存放的时候需要多大空间
+    // 32位机器上的地址：32bit位 - 4byte，所以指针变量的大小是4个字节
+    // 64位机器上的地址：64bit位 - 8byte，所以指针变量的大小是8个字节
 
-    printf("%d",c);
-    
+    printf("%zu\n",sizeof(char*));//8
+    printf("%zu\n",sizeof(short*));//8
+    printf("%zu\n",sizeof(int*));//8
+    printf("%zu\n",sizeof(float*));//8
+    printf("%zu\n",sizeof(double*));//8
+
     return 0;
 }
-
-
